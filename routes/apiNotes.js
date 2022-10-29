3d4efd88aa10e531a8b881a414c5b93d02029887
allenmurfee <allenmurfee123@gmail.com>
2022-10-28 22:15:33 -0400

    working on post request

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -3,3 +3,7 @@ const apiNotes = require("express").Router();
 apiNotes.get("/", (req, res) => {
   readFromFile("../db/db.json").then((data) => res.json(JSON.parse(data)));
 });
+
+apiNotes.post("/", (req, res) => {
+    console.log(req.body);
+})
