c6ee4d6dead22d93b955a550f38aff68d3ce3c90
allenmurfee <allenmurfee123@gmail.com>
2022-11-01 17:18:51 -0400

    delete and add working more efficiently

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -29,13 +29,17 @@ apiNotes.post("/", (req, res) => {
       const parsedNotes = JSON.parse(data);
       parsedNotes.push(newNote);
 
-      fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Review for ${newNote.title} has been written to JSON file`
-            )
+      const writePost = fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes),
+        (err) =>
+          err
+            ? console.error(err)
+            : console.log(
+                `Review for ${newNote.title} has been written to JSON file`
+              )
       );
+      res.json(writePost);
     });
     res.status(201).json("Success");
   } else {
@@ -47,15 +51,18 @@ apiNotes.delete("/:id", (req, res) => {
   const paramId = req.params.id;
   fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
     if (err) throw err;
-    //Need to remove from JSON file
     let parseData = JSON.parse(data);
     let removeNote = parseData.filter((parseData) => parseData.id !== paramId);
     console.log(removeNote);
-    fs.writeFile("./db/db.json", JSON.stringify(removeNote), (err) =>
-      err
-        ? console.error(err)
-        : console.log(`Note has been deleted from JSON file.`)
+    const writeDelete = fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(removeNote),
+      (err) =>
+        err
+          ? console.error(err)
+          : console.log(`Note has been deleted from JSON file.`)
     );
+    res.json(writeDelete);
   });
 });
 module.exports = apiNotes;
